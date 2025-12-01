1156719bb5089835d2e8c40b910a98e6282620f1
MG Potgieter <mgandr@gmail.com>
2025-12-01 11:56:56 +0200

    Day 1 human

---
+++ AdventOfCode2025/Day1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2025
+{
+    public class Day1
+    {
+        public static void SolvePart1(string input)
+        {
+            int maxValue = 100;
+            int number = 50;
+            int zeroCount = 0;
+
+            var steps = input.TrimEnd().Split('\n');
+            foreach (var step in steps) {
+                var direction = step[0];
+                int count = int.Parse(step[1..]);
+                if (direction == 'L') count = count * -1;
+                number = (number + count + maxValue) % maxValue;
+                if (number == 0) zeroCount++;
+            }
+            Console.WriteLine(zeroCount);
+            Console.ReadKey();
+        }
+
+        public static void SolvePart2(string input)
+        {
+            int maxValue = 100;
+            int number = 50;
+            int zeroCount = 0;
+
+            var steps = input.TrimEnd().Split('\n');
+            foreach (var step in steps)
+            {
+                var direction = step[0];
+                int count = int.Parse(step[1..]);
+                for (int i = 0; i < count; i++) {
+                    int stepInc = 1;
+                    if (direction == 'L') stepInc = stepInc * -1;
+                    number += stepInc;
+                    if (number == -1) number = 99;
+                    if (number == 100) number = 0;
+                    if (number == 0) zeroCount++;
+                }
+            }
+            Console.WriteLine(zeroCount);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AdventOfCode2025/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AdventOfCode2025;
+
+Console.WriteLine("Welcome to the Elf on a Shelf Project Management System");
+Console.WriteLine("Please paste input file path:");
+var inputPath = Console.ReadLine();
+var input = File.ReadAllText(inputPath);
+Console.WriteLine("Select operation:");
+Console.WriteLine("1. Day 1 part 1");
+Console.WriteLine("2. Day 1 part 2");
+var operation = Console.Read();
+
+switch (operation)
+{
+	case '1':
+		Day1.SolvePart1(input);
+		break;
+	case '2':
+		Day1.SolvePart2(input); 
+		break;
+	default:
+		break;
+}
+
+Console.ReadKey();
