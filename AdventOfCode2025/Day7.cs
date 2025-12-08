bcfcf4555570e9807604cfcb3ab18fc3e37cf135
MG Potgieter <mgandr@gmail.com>
2025-12-08 14:45:35 +0200

    Day 7 part 2 (ngl I had a peek on the subreddit for ideas)

--- AdventOfCode2025/Day7.cs
+++ AdventOfCode2025/Day7.cs
@@ -37,6 +37,40 @@ namespace AdventOfCode2025
         }
 
         public static void SolvePart2(string input)
-        { }
+        {
+            var lines = input.TrimEnd().Split('\n');
+            long[] timelinesPerCol = new long[lines[0].Length];
+            var starterIndex = lines[0].IndexOf('S');
+            timelinesPerCol[starterIndex] = 1;
+            for (int i = 0; i < lines.Length; i++)
+            {
+                for (int j = 0; j < lines[i].Length; j++)
+                {
+                    if (lines[i][j] == 'S')
+                    {
+                        if (i == lines.Length - 1) break;
+                        if (lines[i + 1][j] == '.')
+                        {
+                            var arr = lines[i + 1].ToCharArray();
+                            arr[j] = 'S';
+                            lines[i + 1] = new string(arr);
+                        }
+                        else if (lines[i + 1][j] == '^')
+                        {
+                            var arr = lines[i + 1].ToCharArray();
+                            arr[j - 1] = 'S';
+                            arr[j + 1] = 'S';
+                            lines[i + 1] = new string(arr);
+
+                            timelinesPerCol[j + 1] += timelinesPerCol[j];
+                            timelinesPerCol[j - 1] += timelinesPerCol[j];
+                            timelinesPerCol[j] = 0;
+
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(timelinesPerCol.Sum());
+        }
     }
 }
